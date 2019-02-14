8f58223d2c3dae09ba310e80f7ea76dc83495ed1
Masatomi KINO <masatomix@ki-no.org>
2019-02-15 01:12:07 +0900

    poolUtil : MySQLの connection poolをココにまとめた Production環境の時だけ、socketpathを設定(Functionsから、GCPのMySQLへ接続する場合の固有の設定？)

---
+++ functions/src/poolUtil.ts
@@ -0,0 +1,22 @@
+import * as mysql from 'promise-mysql'
+import config from './config'
+import { socketPath } from './config'
+
+const mysqlConfig: any = config
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('ProductionEnv...')
+  mysqlConfig.socketPath = socketPath
+}
+
+let pool
+
+export default {
+  getPool() {
+    if (!pool) {
+      console.log('config: ', JSON.stringify(mysqlConfig))
+      pool = mysql.createPool(mysqlConfig)
+    }
+    return pool
+  }
+}
